383ebcc4aa0b58676b77607c96f861cde67bbae9
Horacio Correia <rmcorre@gmail.com>
2020-12-01 22:12:00 -0100

    Add SpringBoot for Web, JPA, and H2

---
+++ src/test/java/org/academiadecodigo/codezillas/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.codezillas.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
