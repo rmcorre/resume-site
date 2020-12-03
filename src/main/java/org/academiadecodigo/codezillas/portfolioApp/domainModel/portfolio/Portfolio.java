93b8278d5f2c678ef426355b44d2962395df2edd
Ross Correia <rmcorre@gmail.com>
2020-12-03 18:27:02 -0100

    Create Portfolio

---
+++ src/main/java/org/academiadecodigo/codezillas/portfolioApp/domainModel/portfolio/Portfolio.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.codezillas.portfolioApp.domainModel.portfolio;
+
+public class Portfolio {
+
+    //***** Make sure you're in dev branch
+    //Do a branch for identity and checkout branch
+    //Stage the identity class
+    //Commit the identity class
+    
+    //***** Make sure you're in dev branch
+    //Do a branch for address and checkout branch
+    //Stage the address class
+    //Commit the address class
+
+    
+    //*****************************************************************************************
+    
+    //***** Make sure you're in dev branch
+    //Do a portfolio branch and checkout branch
+
+    //Add an Identity field to accept an identity from a select input
+
+    //Add an Address field to accept an address from a select input
+}
