e230a2258f65e417908819d2d54015eef5b9a3a2
Ross Correia <rmcorre@gmail.com>
2020-12-17 19:09:00 -0100

    Implement 'role/create.html' and 'role/createConfirmation.html'.

--- src/main/java/org/academiadecodigo/codezillas/portfolioApp/ProjectToDo.java
+++ src/main/java/org/academiadecodigo/codezillas/portfolioApp/ProjectToDo.java
@@ -3,8 +3,24 @@ package org.academiadecodigo.codezillas.portfolioApp;
 public class ProjectToDo {
 
     //Todo:
-    //  1. Implement RoleCtrl
     //  2. Implement role/create.html
     //  3. Modify select element for role on portfolio/create.html
     //  4. Modify PortfolioCtrl process method to add submitted role to portfolio
+
+    /* POSSIBLE SOLUTION TO RETAIN FORM VALUES AFTER SUBMIT
+       https://stackoverflow.com/questions/33208581/how-to-keep-text-value-as-it-is-after-submit-button-is-clicked
+
+       You can add all your attributes received in controller to session attributes so that your attributes and values will be retained.
+
+        Refer this link for using @SessionAttributes in Spring
+
+        http://www.intertech.com/Blog/understanding-spring-mvc-model-and-session-attributes/
+
+        Alternatively, you can use spring form backing object and store it in session
+
+        Refer this link for using spring form backing object
+
+        http://www.tutorialspoint.com/spring/spring_mvc_form_handling_example.htm
+
+    */
 }
