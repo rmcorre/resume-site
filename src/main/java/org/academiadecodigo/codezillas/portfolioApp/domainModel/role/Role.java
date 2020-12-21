17a0ad45d33ab353d307997ea46346444ceeead0
Ross Correia <rmcorre@gmail.com>
2020-12-21 22:53:49 -0100

    Implement ManyToMany association between Role and Skill. Implement RoleTest.

--- src/main/java/org/academiadecodigo/codezillas/portfolioApp/domainModel/role/Role.java
+++ src/main/java/org/academiadecodigo/codezillas/portfolioApp/domainModel/role/Role.java
@@ -2,12 +2,13 @@ package org.academiadecodigo.codezillas.portfolioApp.domainModel.role;
 
 import org.academiadecodigo.codezillas.portfolioApp.domainModel.AbstractModel;
 import org.academiadecodigo.codezillas.portfolioApp.domainModel.portfolio.Portfolio;
+import org.academiadecodigo.codezillas.portfolioApp.domainModel.skill.Skill;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 public class Role extends AbstractModel {
@@ -17,12 +18,33 @@ public class Role extends AbstractModel {
     @ManyToMany
     private final List<Portfolio> portfolioList = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable(
+        name = "role_skill",
+        joinColumns = @JoinColumn(name = "role_id"),
+        inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    private final Set<Skill> skillSet = new HashSet<>();
+
     public String getRole() {
         return role;
     }
 
     public void setRole(String function) {
         this.role = function;
+    }
+
+    public Set<Skill> getSkillSet() {
+        return skillSet;
+    }
+
+    public void addSkill(Skill skill) {
+        skillSet.add(skill);
+        skill.getRoleSet().add(this);
+    }
+
+    public void removeSkill(Skill skill) {
+        skillSet.remove(skill);
+        skill.getRoleSet().remove(this);
     }
 
     @Override
--- src/main/java/org/academiadecodigo/codezillas/portfolioApp/domainModel/skill/Skill.java
+++ src/main/java/org/academiadecodigo/codezillas/portfolioApp/domainModel/skill/Skill.java
@@ -1,14 +1,21 @@
 package org.academiadecodigo.codezillas.portfolioApp.domainModel.skill;
 
 import org.academiadecodigo.codezillas.portfolioApp.domainModel.AbstractModel;
+import org.academiadecodigo.codezillas.portfolioApp.domainModel.role.Role;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Skill extends AbstractModel {
 
     private String skillName;
+
+    @ManyToMany(mappedBy = "skillSet")
+    private final Set<Role> roleSet = new HashSet<>();
 
     public String getSkillName() {
         return skillName;
@@ -16,6 +23,10 @@ public class Skill extends AbstractModel {
 
     public void setSkillName(String skillName) {
         this.skillName = skillName;
+    }
+
+    public Set<Role> getRoleSet() {
+        return roleSet;
     }
 
     @Override
