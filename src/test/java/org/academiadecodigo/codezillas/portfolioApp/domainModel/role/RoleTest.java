9aef7a297f385c988c20770a2cffee191c7a6165
Ross Correia <rmcorre@gmail.com>
2020-12-21 23:33:54 -0100

    Implement remove_skill in RoleTest. Add Role and Skill database transactions to PortfolioApplication command line runner.

--- src/test/java/org/academiadecodigo/codezillas/portfolioApp/domainModel/role/RoleTest.java
+++ src/test/java/org/academiadecodigo/codezillas/portfolioApp/domainModel/role/RoleTest.java
@@ -72,4 +72,36 @@ public class RoleTest {
 
         assertTrue(savedRoleWithSkillAdded.getSkillSet().contains(savedSkill));
     }
+
+    @Test
+    @Transactional
+    @DirtiesContext
+    public void remove_skill() {
+        Role role = new Role();
+        Skill skill = new Skill();
+
+        role.setId(1);
+        role.setVersion(0);
+        role.setCreationTime(new Date());
+        role.setUpdateTime(new Date());
+        role.setRole("Fullstack Developer");
+        roleDAO.saveOrUpdate(role);
+        Role savedRole = roleDAO.find(1);
+
+        skill.setId(1);
+        skill.setVersion(0);
+        skill.setCreationTime(new Date());
+        skill.setUpdateTime(new Date());
+        skill.setSkillName("JPA");
+        skillDAO.saveOrUpdate(skill);
+        Skill savedSkill = skillDAO.find(1);
+
+        savedRole.getSkillSet().add(savedSkill);
+        Role roleWithSkillAdded = roleDAO.find(1);
+
+        roleWithSkillAdded.removeSkill(savedSkill);
+        Role roleWithSkillRemoved = roleDAO.find(1);
+
+        assertEquals(0, roleWithSkillRemoved.getSkillSet().size());
+    }
 }
